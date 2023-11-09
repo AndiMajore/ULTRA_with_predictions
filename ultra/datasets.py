c00a08677f827008f6aa8c338560045dbcc0e928
AndiMajore <andi.majore@googlemail.com>
2023-11-09 20:05:38 +0100

    fixed paths

--- ultra/datasets.py
+++ ultra/datasets.py
@@ -1137,9 +1137,9 @@ class EnvironmentDataset(TransductiveDataset):
     delimiter = ","
     print("Initializing dataset")
     urls = [
-        f'file:///{os.environ["TRAIN_FILE"]}',
-        f'file:///{os.environ["VALID_FILE"]}' ,
-        f'file:///{os.environ["TEST_FILE"]}' ,
+        f'file://{os.environ["TRAIN_FILE"]}',
+        f'file://{os.environ["VALID_FILE"]}' ,
+        f'file://{os.environ["TEST_FILE"]}' ,
         ]
     name = os.environ["DATASET_NAME"]
     
