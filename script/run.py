67d64c797ef3c2dee914cf678b5284b68b86259d
Faren <87604643+Firoozbakht@users.noreply.github.com>
2023-11-09 23:52:28 +0100

    Update run.py

--- script/run.py
+++ script/run.py
@@ -13,7 +13,7 @@ from torch.utils import data as torch_data
 from torch_geometric.data import Data
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-from ultra import tasks
+from ultra import tasks, util
 from ultra.models import Ultra
 
 
@@ -105,7 +105,7 @@ def train_and_validate(cfg, model, train_data, valid_data, device, logger, filte
         if rank == 0:
             logger.warning(separator)
             logger.warning("Evaluate on valid")
-        result = test(cfg, model, valid_data, work_directory, filtered_data=filtered_data, device=device, logger=logger)
+        result = test(cfg, model, valid_data, filtered_data=filtered_data, device=device, logger=logger)
         if result > best_result:
             best_result = result
             best_epoch = epoch
@@ -118,10 +118,9 @@ def train_and_validate(cfg, model, train_data, valid_data, device, logger, filte
 
 
 @torch.no_grad()
-def test(cfg, model, test_data, device, logger, work_directory, filtered_data=None, return_metrics=False):
-    from ultra import util
-    world_size = get_world_size()
-    rank = get_rank()
+def test(cfg, model, test_data, device, logger, filtered_data=None, return_metrics=False):
+    world_size = util.get_world_size()
+    rank = util.get_rank()
 
     test_triplets = torch.cat([test_data.target_edge_index, test_data.target_edge_type.unsqueeze(0)]).t()
     sampler = torch_data.DistributedSampler(test_triplets, world_size, rank)
@@ -142,22 +141,25 @@ def test(cfg, model, test_data, device, logger, work_directory, filtered_data=No
         t_pred = model(test_data, t_batch)
         h_pred = model(test_data, h_batch)
         
-    
+        
         ########################################################### New
+        #print("###########################################")
+        #print(f' t_pred: {t_pred}')
+        #print(t_pred.type())
         #print(f"tail: {t_pred}, head: {h_pred}")
         t_prob = F.softmax(t_pred, dim = 1)
         h_prob = F.softmax(h_pred, dim = 1)
         
-        prob_tail, t_index = torch.max(t_prob,1)
-        prob_head, h_index = torch.max(h_prob,1)
+        _, t_index = torch.max(t_prob,1)
+        _, h_index = torch.max(h_prob,1)
         
         tail_index += [t_index]
         head_index += [h_index]
         
-        probability_tail += [prob_tail]
-        probability_head += [prob_head]
+        probability_tail += [t_prob]
+        probability_head += [h_prob]
+        
         ###########################################
-
         if filtered_data is None:
             t_mask, h_mask = tasks.strict_negative_mask(test_data, batch)
         else:
@@ -187,31 +189,35 @@ def test(cfg, model, test_data, device, logger, work_directory, filtered_data=No
     probability_tail_cat = torch.cat(probability_tail)
     probability_head_cat = torch.cat(probability_head)
     
-    # print(probability_tail_cat)
+    #print(probability_tail_cat.shape)
     # print(probability_head_cat)
     
-
-    # path_dir_pred = os.path.join(work_directory, "prediction")
+    
+    root_path  = "/app"
+    
+    path_dir_pred = os.path.join(root_path, "result/Prediction")
+    n_file_pred = len(os.listdir(path_dir_pred))
     #print("###############################")
 
     #print(os.listdir(path_dir_pred))
     #print(f'n_file_pred: {n_file_pred}')
-
+    n_pred = n_file_pred + 1
+    
     torch.save({'tail_index' : tail_index_cat, 
                 'head_index': head_index_cat},
-               os.path.join(work_directory,"pred.pt"))
+               path_dir_pred + "/pred_" + str(n_pred) + ".pt")
     
     
-    # path_dir_prob  = "/app"
-    # path_dir_prob = os.path.join(path_dir_prob, "Probability")
-    # n_file_prob = len(os.listdir(path_dir_prob))
+
+    path_dir_prob = os.path.join(root_path, "result/Probability")
+    n_file_prob = len(os.listdir(path_dir_prob))
     #print(f'n_file_prob: {n_file_prob}')
 
-    # n_prob = n_file_prob + 1
+    n_prob = n_file_prob + 1
     
     torch.save({'probability_tail' : probability_tail_cat, 
                 'probability_head': probability_head_cat},
-               os.path.join(work_directory , "prob.pt"))
+               path_dir_prob + "/prob_" + str(n_prob) + ".pt")
     ###########################################################
 
     # print(f"ranking: {rankings}")
@@ -288,78 +294,11 @@ def test(cfg, model, test_data, device, logger, work_directory, filtered_data=No
 
     return mrr if not return_metrics else metrics
 
-def load_config(cfg_file, context=None):
-    import jinja2, easydict, yaml
-    with open(cfg_file, "r") as fin:
-        raw = fin.read()
-    template = jinja2.Template(raw)
-    instance = template.render(context)
-    cfg = yaml.safe_load(instance)
-    cfg = easydict.EasyDict(cfg)
-    return cfg
 
-def get_world_size():
-    if dist.is_initialized():
-        return dist.get_world_size()
-    if "WORLD_SIZE" in os.environ:
-        return int(os.environ["WORLD_SIZE"])
-    return 1
-
-def get_rank():
-    if dist.is_initialized():
-        return dist.get_rank()
-    if "RANK" in os.environ:
-        return int(os.environ["RANK"])
-    return 0
-
-def synchronize():
-    if get_world_size() > 1:
-        dist.barrier()
-
-
-def create_working_directory(cfg):
-    import time
-    file_name = "working_dir.tmp"
-    world_size = get_world_size()
-    if cfg.train.gpus is not None and len(cfg.train.gpus) != world_size:
-        error_msg = "World size is %d but found %d GPUs in the argument"
-        if world_size == 1:
-            error_msg += ". Did you launch with `python -m torch.distributed.launch`?"
-        raise ValueError(error_msg % (world_size, len(cfg.train.gpus)))
-    if world_size > 1 and not dist.is_initialized():
-        dist.init_process_group("nccl", init_method="env://")
-
-    working_dir = os.path.join(os.path.expanduser(cfg.output_dir),
-                               cfg.model["class"], cfg.dataset["class"], time.strftime("%Y-%m-%d-%H-%M-%S"))
-
-    # synchronize working directory
-    if get_rank() == 0:
-        with open(file_name, "w") as fout:
-            fout.write(working_dir)
-        os.makedirs(working_dir)
-    synchronize()
-    if get_rank() != 0:
-        with open(file_name, "r") as fin:
-            working_dir = fin.read()
-    synchronize()
-    if get_rank() == 0:
-        os.remove(file_name)
-
-    os.chdir(working_dir)
-    return working_dir
-
-
-def run(args, vars):
-
-    cfg = load_config(args.config, context=vars)
-    working_dir = create_working_directory(cfg)
-    os.environ["WORKDIR"]= working_dir
-    os.environ["TRAIN_FILE"]=cfg.get('train_file')
-    os.environ["TEST_FILE"]=cfg.get('test_file')
-    os.environ["VALID_FILE"]=cfg.get('valid_file')
-    os.environ["DATASET_NAME"]=cfg.get('dataset_name')
-    print(os.environ['TRAIN_FILE'])    
-    from ultra import util
+if __name__ == "__main__":
+    args, vars = util.parse_args()
+    cfg = util.load_config(args.config, context=vars)
+    working_dir = util.create_working_directory(cfg)
 
     torch.manual_seed(args.seed + util.get_rank())
 
@@ -368,48 +307,45 @@ def run(args, vars):
         logger.warning("Random seed: %d" % args.seed)
         logger.warning("Config file: %s" % args.config)
         logger.warning(pprint.pformat(cfg))
-
+    
     task_name = cfg.task["name"]
     dataset = util.build_dataset(cfg)
     device = util.get_device(cfg)
-
+    
     train_data, valid_data, test_data = dataset[0], dataset[1], dataset[2]
     train_data = train_data.to(device)
     valid_data = valid_data.to(device)
     test_data = test_data.to(device)
-
+    
     model = Ultra(
         rel_model_cfg=cfg.model.relation_model,
         entity_model_cfg=cfg.model.entity_model,
     )
+        
 
     if "checkpoint" in cfg and cfg.checkpoint is not None:
         state = torch.load(cfg.checkpoint, map_location="cpu")
         model.load_state_dict(state["model"])
 
-    # model = pyg.compile(model, dynamic=True)
+    #model = pyg.compile(model, dynamic=True)
     model = model.to(device)
-
+    
     if task_name == "InductiveInference":
         # filtering for inductive datasets
         # Grail, MTDEA, HM datasets have validation sets based off the training graph
         # ILPC, Ingram have validation sets from the inference graph
-        # filtering dataset should contain all true edges (base graph + (valid) + test)
+        # filtering dataset should contain all true edges (base graph + (valid) + test) 
         if "ILPC" in cfg.dataset['class'] or "Ingram" in cfg.dataset['class']:
             # add inference, valid, test as the validation and test filtering graphs
-            full_inference_edges = torch.cat(
-                [valid_data.edge_index, valid_data.target_edge_index, test_data.target_edge_index], dim=1)
-            full_inference_etypes = torch.cat(
-                [valid_data.edge_type, valid_data.target_edge_type, test_data.target_edge_type])
-            test_filtered_data = Data(edge_index=full_inference_edges, edge_type=full_inference_etypes,
-                                      num_nodes=test_data.num_nodes)
+            full_inference_edges = torch.cat([valid_data.edge_index, valid_data.target_edge_index, test_data.target_edge_index], dim=1)
+            full_inference_etypes = torch.cat([valid_data.edge_type, valid_data.target_edge_type, test_data.target_edge_type])
+            test_filtered_data = Data(edge_index=full_inference_edges, edge_type=full_inference_etypes, num_nodes=test_data.num_nodes)
             val_filtered_data = test_filtered_data
         else:
             # test filtering graph: inference edges + test edges
             full_inference_edges = torch.cat([test_data.edge_index, test_data.target_edge_index], dim=1)
             full_inference_etypes = torch.cat([test_data.edge_type, test_data.target_edge_type])
-            test_filtered_data = Data(edge_index=full_inference_edges, edge_type=full_inference_etypes,
-                                      num_nodes=test_data.num_nodes)
+            test_filtered_data = Data(edge_index=full_inference_edges, edge_type=full_inference_etypes, num_nodes=test_data.num_nodes)
 
             # validation filtering graph: train edges + validation edges
             val_filtered_data = Data(
@@ -418,66 +354,21 @@ def run(args, vars):
             )
     else:
         # for transductive setting, use the whole graph for filtered ranking
-        filtered_data = Data(edge_index=dataset._data.target_edge_index, edge_type=dataset._data.target_edge_type,
-                             num_nodes=dataset[0].num_nodes)
+        filtered_data = Data(edge_index=dataset._data.target_edge_index, edge_type=dataset._data.target_edge_type, num_nodes=dataset[0].num_nodes)
         val_filtered_data = test_filtered_data = filtered_data
-
+    
     val_filtered_data = val_filtered_data.to(device)
     test_filtered_data = test_filtered_data.to(device)
-
-    train_and_validate(cfg, model, train_data, valid_data, filtered_data=val_filtered_data, device=device,
-                       logger=logger)
-    # Temporarily disabled until we actually train
-    # if util.get_rank() == 0:
-    #     logger.warning(separator)
-    #     logger.warning("Evaluate on valid")
-    # test(cfg, model, valid_data, filtered_data=val_filtered_data, device=device, logger=logger)
+    
+    train_and_validate(cfg, model, train_data, valid_data, filtered_data=val_filtered_data, device=device, logger=logger)
+    if util.get_rank() == 0:
+        logger.warning(separator)
+        logger.warning("Evaluate on valid")
+    test(cfg, model, valid_data, filtered_data=val_filtered_data, device=device, logger=logger)
     if util.get_rank() == 0:
         logger.warning(separator)
         logger.warning("Evaluate on test")
-    test(cfg, model, test_data, work_directory=working_dir, filtered_data=test_filtered_data, device=device, logger=logger)
-
-def detect_variables(cfg_file):
-    import jinja2
-    from jinja2 import meta
-    with open(cfg_file, "r") as fin:
-        raw = fin.read()
-    env = jinja2.Environment()
-    tree = env.parse(raw)
-    vars = meta.find_undeclared_variables(tree)
-    return vars
-
-def literal_eval(string):
-    import ast
-    try:
-        return ast.literal_eval(string)
-    except (ValueError, SyntaxError):
-        return string
-
-
-def parse_args():
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--config", help="yaml configuration file", required=True)
-    parser.add_argument("-s", "--seed", help="random seed for PyTorch", type=int, default=1024)
-
-    args, unparsed = parser.parse_known_args()
-    # get dynamic arguments defined in the config file
-    vars = detect_variables(args.config)
-    parser = argparse.ArgumentParser()
-    for var in vars:
-        parser.add_argument("--%s" % var, required=True)
-    vars = parser.parse_known_args(unparsed)[0]
-    vars = {k: literal_eval(v) for k, v in vars._get_kwargs()}
-
-    return args, vars
-
-
-if __name__ == "__main__":
-    args, vars = parse_args()
-    run(args, vars)
-
-
+    test(cfg, model, test_data, filtered_data=test_filtered_data, device=device, logger=logger)
     
     
     
